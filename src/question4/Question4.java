dae3de5e7a04338123f1596eb08441dac9077bc8
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-08-19 15:17:34 +0530

    find int positive or negative or zero

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,7 +1,18 @@
 package question4;
 
+import java.util.Scanner;
+
 public class Question4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        if (a == 0)
+            System.out.println("Zero");
+        else if (a > 0)
+            System.out.println("positive");
+        else if (a < 0)
+            System.out.println("negative");
+        scanner.close();
 
     }
 }
