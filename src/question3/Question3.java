379496e6de0987e65b1b42023e38d68f1c174c3a
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-08-19 15:08:17 +0530

    add,multiply,devide of two integer

--- src/question3/Question3.java
+++ src/question3/Question3.java
@@ -1,7 +1,23 @@
 package question3;
 
+import java.util.Scanner;
+
 public class Question3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        //c=a+b
+        System.out.println("add-" + a + b);
+        //a*b
+        System.out.println("multiply-" + a * b);
+        //a-b
+        if (a > b)
+            System.out.println(a - b);
+        else System.out.println(b - a);
+        //a/b
+        System.out.println("devide-" + a / b);
 
+        scanner.close();
     }
 }
