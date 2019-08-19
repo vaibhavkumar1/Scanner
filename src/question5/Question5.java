150559e4bbb45ca0f9aadff5501c31dc3b379d37
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-08-19 15:32:01 +0530

    char input method

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -1,7 +1,16 @@
 package question5;
 
+import java.util.Scanner;
+
 public class Question5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char a = scanner.next().charAt(0);
+        System.out.println(a);
+        scanner.close();
+
+
+
 
     }
 }
