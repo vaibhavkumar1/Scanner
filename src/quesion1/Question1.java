c54dc664608545a016976f07914b20861b4b0363
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-08-19 14:29:08 +0530

    add input for variable:(username,String)

--- src/quesion1/Question1.java
+++ src/quesion1/Question1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextline().trim();
         scanner.close();
 
 
