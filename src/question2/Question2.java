d293a78079c4455d67b74f301d2e10f718ff4dd5
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-08-19 14:46:33 +0530

    define two new string variables:value1,value2 and input

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -1,7 +1,15 @@
 package question2;
 
+import java.util.Scanner;
+
 public class Question2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value1 = scanner.next().trim();
+        String value2 = scanner.next().trim();
+        System.out.println(value1 + value2);
+        scanner.close();
+
 
     }
 }
