d4de6ba185935d873ac79b91ad9eeec6da931944
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-08-19 14:32:37 +0530

    add output statement

--- src/quesion1/Question1.java
+++ src/quesion1/Question1.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String username = scanner.nextline().trim();
+        String username = scanner.nextLine().trim();
+        System.out.println("Welcome" + username + "!");
         scanner.close();
 
 
